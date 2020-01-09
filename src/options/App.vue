fceb8098d5438ee3c5c128b4470e4e7c4f212e87
qixinghui <869662648@qq.com>
2020-01-09 21:54:21 +0800

    feat: login page

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="content">
-    
+  <div class="content flex-center">
+    <div class="login-content flex-center">
+      <img class="logo" src="../icons/logo.png" alt />
+    </div>
   </div>
 </template>
 
@@ -14,6 +16,27 @@ export default {
 .content {
   width: 700px;
   height: 500px;
-  background-color: red;
+
+  .login-content {
+    width: 510px;
+    height: 290px;
+    margin: 0 auto;
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 1);
+    text-align: center;
+    box-shadow: 0px 4px 20px 2px rgba(0, 0, 0, 0.2);
+    border: 1px solid rgba(255, 255, 255, 0);
+    flex-direction: column;
+
+    .logo {
+      width: 50px;
+      height: 50px;
+      margin-top: 20px;
+    }
+  }
+}
+.flex-center {
+  display: flex;
+  align-items: center;
 }
 </style>
