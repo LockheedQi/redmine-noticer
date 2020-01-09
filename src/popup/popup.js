6ac759e4405dece82a4a5b921f51449057ad7e38
lockheed <869662648@qq.com>
2020-01-09 13:41:15 +0800

    chore: include element-ui

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import store from '../store'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 global.browser = require('webextension-polyfill')
 Vue.prototype.$browser = global.browser
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
