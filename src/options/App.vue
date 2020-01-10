3725ba41933f78234095b4d8473d7ade950020c4
lockheed <869662648@qq.com>
2020-01-10 09:17:39 +0800

    feat: login page

--- src/options/App.vue
+++ src/options/App.vue
@@ -2,13 +2,33 @@
   <div class="content flex-center">
     <div class="login-content flex-center">
       <img class="logo" src="../icons/logo.png" alt />
+      <div class="url flex-center">
+        <div class="title">Redmine URL</div>
+        <el-input
+          placeholder="请输入Redmine地址"
+          v-model="redmineUrl">
+        </el-input>
+      </div>
+      <div class="url flex-center">
+        <div class="title">API Key</div>
+        <el-input
+          placeholder="请输入API access key"
+          v-model="accessKey">
+        </el-input>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data(){
+    return{
+      redmineUrl:'',
+      accessKey:''
+    }
+  }
 };
 </script>
 
@@ -32,6 +52,26 @@ export default {
       width: 50px;
       height: 50px;
       margin-top: 20px;
+    }
+
+    .url{
+      margin-top: 24px;
+      
+      .title{
+        width: 100px;
+        height: 40px;
+        line-height: 40px;
+        white-space: nowrap;
+        color: rgba(16, 16, 16, 1);
+        font-size: 14px;
+        text-align: right;
+        font-family: SourceHanSansSC-regular;
+      }
+      .el-input{
+        margin-left: 28px;
+        width: 313px;
+        // border: 1px solid rgba(220, 223, 230, 1);
+      }
     }
   }
 }
--- src/options/options.js
+++ src/options/options.js
@@ -1,8 +1,11 @@
 global.browser = require('webextension-polyfill')
 
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
