332ce2ddeeb71922002d66cd90c359b1564473ef
qixinghui <869662648@qq.com>
2020-01-13 21:46:20 +0800

    feat: popup page

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -2,11 +2,29 @@
   <div class="popup-content">
     <el-table :data="tableData" style="width: 100%" :cell-class-name="cellClass">
       <el-table-column prop="id" label="#" width="66"></el-table-column>
-      <el-table-column prop="tracker.name" label="跟踪" width="60"></el-table-column>
+      <el-table-column prop="tracker.name" label="跟踪" width="60">
+        <template slot-scope="scope">
+          <div class="tracker-icon">
+            <img v-if="scope.row.tracker.id == 5" class="tracker" src="../../../icons/task.png" alt />
+            <img
+              v-else-if="scope.row.tracker.id == 6"
+              class="tracker"
+              src="../../../icons/bug.png"
+              alt
+            />
+            <img
+              v-else-if="scope.row.tracker.id == 8"
+              class="tracker"
+              src="../../../icons/works.png"
+              alt
+            />
+          </div>
+        </template>
+      </el-table-column>
       <el-table-column prop="project.name" label="项目" width="120"></el-table-column>
       <el-table-column prop="status.name" label="状态" width="90"></el-table-column>
       <el-table-column prop="description" label="主题"></el-table-column>
-      <el-table-column prop="updated_on" :formatter="dateFormatter" label="更新于" width="92"></el-table-column>
+      <el-table-column prop="updated_on" :formatter="dateFormatter" label="更新于" width="98"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -38,11 +56,11 @@ export default {
           },
           author: {
             id: 14,
-            name: "祁兴辉"
+            name: "lockheed"
           },
           assigned_to: {
             id: 14,
-            name: "祁兴辉"
+            name: "lockheed"
           },
           subject: "2020年1月工作任务",
           description:
@@ -81,11 +99,11 @@ export default {
           },
           author: {
             id: 14,
-            name: "祁兴辉"
+            name: "lockheed"
           },
           assigned_to: {
             id: 14,
-            name: "祁兴辉"
+            name: "lockheed"
           },
           subject: "2020年1月工作任务",
           description: "我是一个bug",
@@ -113,7 +131,7 @@ export default {
       }
     },
     dateFormatter(row, column, cellValue, index) {
-      return (this.tableData[0].updated_on = moment(cellValue).format(
+      return (moment(cellValue).format(
         "YYYY-MM-DD HH:mm:ss"
       ));
     }
@@ -124,10 +142,16 @@ export default {
 <style lang="scss" scoped>
 .popup-content {
   width: 780px;
-  background: red;
-
-  .cellClass {
-    background: red;
+  margin-bottom: 20px;
+  .tracker-icon {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    img {
+      margin: auto;
+      width: 20px;
+      height: 20px;
+    }
   }
 }
 </style>
